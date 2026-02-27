748bcdbc54de7f1121dada4e356d8f3844203bf9
itsmesunky <itisp_06@naver.com>
2026-02-27 16:30:59 +0900

    chore: trpc 초기 설정

---
+++ src/trpc/init.ts
@@ -0,0 +1,27 @@
+import { cache } from "react";
+
+import { headers } from "next/headers";
+
+import { TRPCError, initTRPC } from "@trpc/server";
+
+import { auth } from "@/lib/auth";
+
+export const createTRPCContext = cache(async () => {
+  return { userId: "user_123" };
+});
+
+const t = initTRPC.create({});
+export const createTRPCRouter = t.router;
+export const createCallerFactory = t.createCallerFactory;
+export const baseProcedure = t.procedure;
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "Unauthorized" });
+  }
+
+  return next({ ctx: { ...ctx, auth: session } });
+});
