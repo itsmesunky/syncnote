0bbc1c44dc8883f26bbdd0e5bb85bf4bdef131e5
선우 <99164690+itsmesunky@users.noreply.github.com>
2026-03-03 22:26:05 +0900

    feat: 회의 관련 프로시저 추가

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,4 +1,8 @@
+import { meetingsRouter } from "@/modules/meetings/server/procedure";
+
 import { createTRPCRouter } from "../init";
 
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  meetings: meetingsRouter,
+});
 export type AppRouter = typeof appRouter;
