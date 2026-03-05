7cff74218c5fe4b474b9a0154586dc3bb8cac58b
선우 <itisp_06@naver.com>
2026-03-05 15:37:56 +0900

    style: auth 관련 페이지 스타일 추가

--- src/modules/auth/ui/view/sign-in-view.tsx
+++ src/modules/auth/ui/view/sign-in-view.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -64,57 +65,71 @@ export const SignInView = () => {
         <CardContent className="grid p-0 md:grid-cols-2">
           <div>
             <Form {...methods}>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="grid gap-3">
-                  <FormInput
-                    type="email"
-                    fieldName="email"
-                    control={control}
-                    label="이메일"
-                    placeholder="이메일을 입력해 주세요."
-                  />
+              <form onSubmit={handleSubmit(onSubmit)} className="p-6 md:p-8">
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-3">
+                    <FormInput
+                      type="email"
+                      fieldName="email"
+                      control={control}
+                      label="이메일"
+                      placeholder="이메일을 입력해 주세요."
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <FormInput
+                      type="password"
+                      fieldName="password"
+                      control={control}
+                      label="비밀번호"
+                      placeholder="비밀번호를 입력해 주세요."
+                    />
+                  </div>
+                  {!!error && (
+                    <Alert className="bg-destructive/10 border-none">
+                      <OctagonAlertIcon className="h-4 w-4 text-destructive!" />
+                      <AlertTitle>{error}</AlertTitle>
+                    </Alert>
+                  )}
+                  <Button type="submit" className="w-full" disabled={pending}>
+                    로그인
+                  </Button>
+                  <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                    <span className="bg-card text-muted-foreground relative z-10 px-2">
+                      간편 로그인
+                    </span>
+                  </div>
+                  <div className="grid grid-cols-2 gap-4">
+                    {socialLoginMap.map(({ provider, icon: Icon }) => (
+                      <SocialLoginButton
+                        key={provider}
+                        provider={provider}
+                        icon={<Icon />}
+                        onSuccess={() => {
+                          setPending(false);
+                          setError(null);
+                          router.replace("/");
+                        }}
+                        onError={(code) => {
+                          setPending(false);
+                          setError(errorMap[code]);
+                        }}
+                      />
+                    ))}
+                  </div>
+                  <div className="text-center text-sm">
+                    계정이 없으신가요?{" "}
+                    <Link href="/sign-up" className="underline underline-offset-4">
+                      회원가입
+                    </Link>
+                  </div>
                 </div>
-                <div className="grid gap-3">
-                  <FormInput
-                    type="password"
-                    fieldName="password"
-                    control={control}
-                    label="비밀번호"
-                    placeholder="비밀번호를 입력해 주세요."
-                  />
-                </div>
-                {!!error && (
-                  <Alert className="bg-destructive/10 border-none">
-                    <OctagonAlertIcon className="h-4 w-4 text-destructive!" />
-                    <AlertTitle>{error}</AlertTitle>
-                  </Alert>
-                )}
-                <Button type="submit" className="w-full" disabled={pending}>
-                  로그인
-                </Button>
               </form>
             </Form>
-            <div>
-              {socialLoginMap.map(({ provider, icon: Icon }) => (
-                <SocialLoginButton
-                  key={provider}
-                  provider={provider}
-                  icon={<Icon />}
-                  onSuccess={() => {
-                    setPending(false);
-                    setError(null);
-                    router.replace("/");
-                  }}
-                  onError={(code) => {
-                    setPending(false);
-                    setError(errorMap[code]);
-                  }}
-                />
-              ))}
-            </div>
           </div>
           <div className="relative hidden md:flex flex-col gap-y-4 items-center justify-center">
-            TODO: 로고 삽입
+            <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
+            <p className="text-2xl font-semibold text-white">SYNCNOTE</p>
           </div>
         </CardContent>
       </Card>
